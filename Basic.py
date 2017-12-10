6d6382aa667b10400da6e8fb5efd88d8daf6a78b
Lopez-Samuel <lopez.samuel.4196@gmail.com>
2017-12-10 13:58:29 -0500

    Update Basic.py

--- Basic.py
+++ Basic.py
@@ -172,6 +172,62 @@ def like():
     conn.commit()
     cursor.close()
     return render_template('home.html')
+@app.route('/add_Friend', methods = ['GET', 'POST'])
+def add_Friend():
+    username = session['username']
+    friendGroup = request.form['FriendGroup']
+    firstName = request.form['First Name']
+    lastName = request.form['Last Name']
+    description = request.form['Description']
+    cursor = conn.cursor()
+    query = 'SELECT username FROM Person WHERE first name  = %s AND last name = %s'
+    cursor.execute(query,(firstName, lastName))
+    cursor.close()
+    queryCheck = 'COUNT * FROM Member HAVING username = %s AND group_name = %s AND username_creator = %s '
+    cursor.execute(queryCheck,(query, friendGroup, username))
+    error = None
+    if (queryCheck != 1 ):
+        error ='More than one user with name ' firstName ' ' lastName 'exists in friend group ' friendGroup'
+        return redirect(url_for('home'), error= error)
+    else
+        cursor = conn.cursor()
+        addFriendQuery= 'INSERT INTO `Member` (`username`, `group_name`, `username_creator`) VALUES (%s,%s,%s)'
+        cursor.execute(addFriendQuery, (friendGroup,query,username))
+        cursor.close()
+        return redirect(url_for('home'))
+
+
+@app.route('/addFriendgroup', methods = ['POST'])
+def addFriendgroup():
+    username = session['username']
+    friendGroup = request.form['Friendgroup']
+    friend = request.form['Friend']
+    query = 'SELECT '
+    
+
+@app.route('/addComment/<int:line_id>', methods=['GET', 'POST'])
+def addComment(line_id):
+    id=line_id
+    username=session['username']
+    cursor=conn.cursor()
+    comment=request.form['comment']
+    query='INSERT INTO comment(`id`, `username`, `comment_text`) VALUES (%s, %s, %s)'
+    cursor.execute(query, (id, username, comment))
+    conn.commit()
+    cursor.close()
+    return redirect(url_for('home'))
+
+@app.route('/like/<int:line_id>', methods=['GET', 'POST'])
+def like(line_id):
+    id=line_id
+    username=session['username']
+    cursor=conn.cursor()
+    query='INSERT INTO likes(`username`, `id`) VALUES (%s, %s)'
+    cursor.execute(query, (username, id))
+    conn.commit()
+    cursor.close()
+    return redirect(url_for('home'))
+
 
 app.secret_key = 'x53467Dbahb2!23'
 #Run the app on localhost port 5000
