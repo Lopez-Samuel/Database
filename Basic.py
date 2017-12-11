34b9268bee62e8c96a4e7e7a14f167c91788cda6
Lopez-Samuel <lopez.samuel.4196@gmail.com>
2017-12-10 21:11:21 -0500

    Update Basic.py

--- Basic.py
+++ Basic.py
@@ -15,25 +15,23 @@ conn = pymysql.connect(host='localhost',
                        charset='latin1',
                        cursorclass=pymysql.cursors.DictCursor)
 
-# Define a route to hello function
+#Define a route to hello function
 @app.route('/')
 def hello():
-    return render_template('index.html')
+	return render_template('index.html')
 
-
-# Define route for login
+#Define route for login
 @app.route('/login')
 def login():
-    return render_template('login.html')
+	return render_template('login.html')
 
-
-# Define route for register
+#Define route for register
 @app.route('/register')
 def register():
-    return render_template('register.html')
+	return render_template('register.html')
 
 
-# Authenticates the login
+#Authenticates the login
 @app.route('/loginAuth', methods=['GET', 'POST'])
 def loginAuth():
     # grabs information from the forms
@@ -64,8 +62,7 @@ def loginAuth():
         error = 'Invalid login or username'
         return render_template('login.html', error=error)
 
-
-# Authenticates the register
+#Authenticates the register
 @app.route('/registerAuth', methods=['GET', 'POST'])
 def registerAuth():
     # grabs information from the forms
@@ -103,36 +100,39 @@ def registerAuth():
 
 @app.route('/post', methods=['GET', 'POST'])
 def post():
-    username = session['username']
-    cursor = conn.cursor()
-    name = request.form['content name']
-    filepath = request.form['filepath']
-    query = 'INSERT INTO content (`username`, `file_path`, `content_name`, `public`) VALUES (%s, %s, %s,1)'
+    username=session['username']
+    cursor=conn.cursor()
+    name=request.form['content name']
+    filepath=request.form['filepath']
+    query='INSERT INTO content (`username`, `file_path`, `content_name`, `public`) VALUES (%s, %s, %s,1)'
     cursor.execute(query, (username, filepath, name))
     conn.commit()
     cursor.close()
     return redirect(url_for('home'))
 
-
-@app.route('/home', methods=['GET', 'POST'])
+@app.route('/home', methods = ['GET', 'POST'])
 def home():
     username = session['username']
     time = datetime.datetime.now()
     cursor = conn.cursor()
+    fg = 'SELECT group_name, username, description FROM friendgroup WHERE username = %s'
+    cursor.execute(fg, (username))
+    friendGroup = cursor.fetchall()
     query = 'SELECT COUNT(likes.username) as num, content.id, timest, content_name, content.username FROM content LEFT JOIN likes ON content.id= likes.id GROUP BY content.id'
     cursor.execute(query)
     data = cursor.fetchall()
+    query3='SELECT tag.id, username_tagger, content_name FROM tag JOIN content ON tag.id=content.id WHERE tag.username_taggee= %s AND tag.status= "0"'
+    cursor.execute(query3, (username))
+    pending_tags=cursor.fetchall()
     cursor.close()
-    return render_template('home.html', username=username, content=data)
-
+    return render_template('home.html', username = username, content = data, friendGroup = friendGroup, pending_tags = pending_tags)
 
 @app.route('/logout')
 def logout():
-    session.pop('username')
-    return redirect('/')
+	session.pop('username')
+	return redirect('/')
 
-
-@app.route('/add_Friend', methods=['GET', 'POST'])
+@app.route('/add_Friend', methods = ['GET', 'POST'])
 def add_Friend():
     username = session['username']
     friendGroup = request.form['FriendGroup']
@@ -141,74 +141,83 @@ def add_Friend():
     description = request.form['Description']
     cursor = conn.cursor()
     query = 'SELECT username FROM Person WHERE first name  = %s AND last name = %s'
-    cursor.execute(query, (firstName, lastName))
+    cursor.execute(query,(firstName, lastName))
     cursor.close()
     queryCheck = 'COUNT * FROM Member HAVING username = %s AND group_name = %s AND username_creator = %s '
-    cursor.execute(queryCheck, (query, friendGroup, username))
+    cursor.execute(queryCheck,(query, friendGroup, username))
     error = None
-    if (queryCheck != 1):
-        error = 'More than one user with name ' + firstName + ' ' + lastName + 'exists in friend group ' + friendGroup
-        return redirect(url_for('home'), error=error)
+    if (queryCheck != 1 ):
+        error ='More than one user with name ' + firstName  + ' ' + lastName + 'exists in friend group ' + friendGroup
+        return redirect(url_for('home'), error= error)
     else:
         cursor = conn.cursor()
-        addFriendQuery = 'INSERT INTO `Member` (`username`, `group_name`, `username_creator`) VALUES (%s,%s,%s)'
-        cursor.execute(addFriendQuery, (friendGroup, query, username))
+        addFriendQuery= 'INSERT INTO `Member` (`username`, `group_name`, `username_creator`) VALUES (%s,%s,%s)'
+        cursor.execute(addFriendQuery, (friendGroup,query,username))
         cursor.close()
         return redirect(url_for('home'))
 
 
-@app.route('/addFriendgroup', methods=['POST'])
+@app.route('/addFriendGroup', methods = ['POST'])
 def addFriendgroup():
     username = session['username']
-    friendGroup = request.form['Friendgroup']
+    friendGroup = request.form['Friend Group']
     friend = request.form['Friend']
     description = request.form['Description']
     cursor = conn.cursor()
     query = 'INSERT INTO `FriendGroup` (`group_name`, `username`, `description`) VALUES (%s, %s, %s)'
-    cursor.execute(friendGroup, username, description)
+    cursor.execute(query, (friendGroup, username, description))
+    conn.commit()
+    query2 = 'INSERT INTO `Member` (`username`, `group_name`, `username_creator`) VALUES (%s, %s, %s)'
+    cursor.execute(query2, (friend, friendGroup, username))
     conn.commit()
     cursor.close()
     return redirect(url_for('home'))
-
-
+    
 @app.route('/addComment/<int:line_id>', methods=['GET', 'POST'])
 def addComment(line_id):
-    id = line_id
-    username = session['username']
-    cursor = conn.cursor()
-    comment = request.form['comment']
-    query = 'INSERT INTO comment(`id`, `username`, `comment_text`) VALUES (%s, %s, %s)'
+    id=line_id
+    username=session['username']
+    cursor=conn.cursor()
+    comment=request.form['comment']
+    query='INSERT INTO comment(`id`, `username`, `comment_text`) VALUES (%s, %s, %s)'
     cursor.execute(query, (id, username, comment))
     conn.commit()
     cursor.close()
     return redirect(url_for('home'))
 
-
 @app.route('/like/<int:line_id>', methods=['GET', 'POST'])
 def like(line_id):
-    id = line_id
-    username = session['username']
-    cursor = conn.cursor()
-    query = 'INSERT INTO likes(`username`, `id`) VALUES (%s, %s)'
+    id=line_id
+    username=session['username']
+    cursor=conn.cursor()
+    query='INSERT INTO likes(`username`, `id`) VALUES (%s, %s)'
     cursor.execute(query, (username, id))
     conn.commit()
     cursor.close()
     return redirect(url_for('home'))
 
+# @app.route('/viewFriendGroup/group_name', methods = ['GET', 'POST'])
+# def viewFriendGroup(group_name):
+#     group=group_name
+#     username = session['username']
+#     cursor = conn.cursor()
+#     query = 'SELECT username FROM member WHERE group_name = %s'
+#     cursor.execute(query, (group))
+#     data = cursor.fetchall()
+#     return redirect(url_for('home'), line = data)
 
-@app.route('/tag/<int:line_id>', methods=['GET', 'POST'])
-def tag(line_id):
-    id = line_id
-    taggee = session['username_taggee']
-    tagger = request.form['username_tagger']
+@app.route('/tag/<int:data_id>', methods=['GET', 'POST'])
+def tag(data_id):
+    id = data_id
+    username = session['username']
+    taggee = request.form['Tag']
     cursor = conn.cursor()
-    query = 'INSERT INTO Tag(`taggee`,`tagger`, `id`) VALUES (%s, %s)'
-    cursor.execute(query, (taggee, tagger, id))
+    query = 'INSERT INTO `Tag` (`id`, `username_tagger`, `username_taggee`, `status`) VALUES (%s, %s, %s, 0)'
+    cursor.execute(query, (id,username,taggee))
     conn.commit()
     cursor.close()
     return redirect(url_for('home'))
-
-
+    
 @app.route('/view/<int:line_id>', methods=['GET', 'POST'])
 def view(line_id):
     id = line_id
@@ -217,7 +226,7 @@ def view(line_id):
     query = "SELECT count(*) as num FROM likes WHERE id=%s GROUP BY id"
     cursor.execute(query, (id))
     number_of_likes = cursor.fetchone()
-    query = "SELECT file_path, username, timest, content_name FROM content WHERE id=%s"
+    query = "SELECT id, file_path, username, timest, content_name FROM content WHERE id=%s"
     cursor.execute(query, (id))
     line = cursor.fetchone()
     query = "SELECT comment_text FROM comment WHERE id=%s"
@@ -225,12 +234,38 @@ def view(line_id):
     comments = cursor.fetchall()
     conn.commit()
     cursor.close()
-    return render_template('view.html', username=username, id=line_id, data=line, number_of_likes=number_of_likes,
-                           comments=comments)
+    return render_template('view.html', username=username, id=line_id, data=line, number_of_likes=number_of_likes, comments=comments)
+
+@app.route('/approve/<un>/<un_tagger>/<int:line_id>', methods=['GET', 'POST'])
+def approve(un, un_tagger, line_id):
+    cursor=conn.cursor()
+    query="UPDATE tag set status='1' WHERE tag.username_tagger=%s AND tag.Username_taggee=%s AND tag.id=%s"
+    cursor.execute(query, (un_tagger, un, line_id))
+    conn.commit()
+    cursor.close()
+    return redirect(url_for('home'))
+
+@app.route('/ignore/<un>/<un_tagger>/<int:line_id>', methods=['GET', 'POST'])
+def ignore(un, un_tagger, line_id):
+    cursor=conn.cursor()
+    query="UPDATE tag  set status='0' WHERE tag.username_tagger=%s AND tag.Username_taggee=%s AND tag.id=%s"
+    cursor.execute(query, (un_tagger, un, line_id))
+    conn.commit()
+    cursor.close()
+    return redirect(url_for('home')) 
+
+@app.route('/deny/<un>/<un_tagger>/<int:line_id>', methods=['GET', 'POST'])
+def delete(un, un_tagger, line_id):
+    cursor=conn.cursor()
+    query="DELETE FROM tag WHERE tag.username_tagger=%s AND tag.Username_taggee=%s AND tag.id=%s"
+    cursor.execute(query, (un_tagger, un, line_id))
+    conn.commit()
+    cursor.close()
+    return redirect(url_for('home'))
 
 app.secret_key = 'x53467Dbahb2!23'
-# Run the app on localhost port 5000
-# debug = True -> you don't have to restart flask
-# for changes to go through, TURN OFF FOR PRODUCTION
+#Run the app on localhost port 5000
+#debug = True -> you don't have to restart flask
+#for changes to go through, TURN OFF FOR PRODUCTION
 if __name__ == "__main__":
-    app.run('127.0.0.1', 5000, debug=True)
+	app.run('127.0.0.1', 5000, debug = True)
